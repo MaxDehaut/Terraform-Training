f2db157c7a87be57f3a9cf32b410156186d6fe00
Max Dehaut <mdehaut@live.com>
2021-09-05 12:39:32 +0100

    ex4.04

--- solutions/section-4/ex03/database/main.tf
+++ solutions/section-4/ex03/database/main.tf
@@ -1,16 +1,20 @@
 
+resource "random_pet" "login" {
+  length = 8
+}
+
 resource "random_password" "password" {
-    length= 16
-    special          = true
-    override_special = "_%@/'\""
+  length           = 16
+  special          = true
+  override_special = "_%@/'\""
 }
 
 # SQL Server
 resource "azurerm_sql_server" "training" {
   name                         = "sql-${var.suffix}"
-  resource_group_name          = "${var.rg.name}"
-  location                     = "${var.rg.location}"
+  resource_group_name          = var.rg.name
+  location                     = var.rg.location
   version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
+  administrator_login          = random_pet.login.result
   administrator_login_password = random_password.password.result
 }
--- solutions/section-4/ex03/networking/main.tf
+++ solutions/section-4/ex03/networking/main.tf
@@ -16,6 +16,11 @@ resource "azurerm_virtual_network" "training" {
   resource_group_name = azurerm_resource_group.training.name
   location            = azurerm_resource_group.training.location
   address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    "environment" = "training"
+    "costalloc"   = "${var.costalloc}"
+  }
 }
 
 # Subnet
