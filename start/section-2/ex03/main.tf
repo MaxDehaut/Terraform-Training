602767e3d3b5dffe5eb49d3e977b7ddcb4409a67
Max Dehaut <mdehaut@live.com>
2021-09-09 10:13:42 +0200

    ex 4.08

--- start/section-2/ex03/main.tf
+++ start/section-2/ex03/main.tf
@@ -1,20 +1,20 @@
 terraform {
-  required_version = ">= 0.15"
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
-  }
+required_version = ">= 0.15"
+required_providers {
+local = {
+source  = "hashicorp/local"
+version = "~> 2.0"
+}
+}
 }
 
 resource "local_file" "Britney" {
-    filename = "I_did_it_again.txt"
-    content = <<-EOT
-      'Cause to lose all my senses
+filename = "I_did_it_again.txt"
+content  = <<-EOT
+'Cause to lose all my senses
 
-      That is just so typically me 
-      You see, my problem is this, I'm dreaming away
-      Can't you see I'm a fool in so many ways?
-    EOT
+That is just so typically me 
+You see, my problem is this, I'm dreaming away
+Can't you see I'm a fool in so many ways?
+EOT
 }
